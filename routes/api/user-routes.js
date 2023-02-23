c1c262bea9229ce74b345da65e24741371d93de3
Mohamed <abdi2k24@gmail.com>
2023-02-23 12:52:10 -0600

    updated and added user routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,1 +1,17 @@
 const router = require('express').Router();
+
+const {
+    createUser,
+    getAllUser,
+    getUserById,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+ } = require('../../controllers/user-controller');
+
+ router.route( "/" ).get(getAllUser).post(createUser);
+ router.route( "/:id" ).get(getSingleUser).put(updateUser).delete(deleteUser);
+
+
+ module.exports = router;
