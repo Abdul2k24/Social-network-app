ad38483214e6ca8681821d928032ca157739606d
Mohamed <abdi2k24@gmail.com>
2023-02-23 01:19:28 -0600

    completed mongoose reaction schema

---
+++ models/Reaction.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat.js');
+
+const reactionSchema = new Schema(
+  {
+    reactionBody: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: timestamp => dateFormat(timestamp)
+    }
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+module.exports = reactionSchema;
